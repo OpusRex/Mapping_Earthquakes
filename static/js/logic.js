f9b9d2851a081f3d7f353af9c0f60cd9ce5070a6
OpusRex <48140318+OpusRex@users.noreply.github.com>
2020-04-12 16:56:09 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,51 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// //Center of map in Los Angles
-// // Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([36.1733, -120.1794], 7);
-
-
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-
-// //Single Line start
-// // Coordinates for each point to be used in the line.
-// let line = [
-// 	[33.9416, -118.4085],
-// 	[37.6213, -122.3790]
-//   ];
-
-//   // Create a polyline using the line coordinates and make the line red.
-// L.polyline(line, {
-// 	color: "red"
-//   }).addTo(map);
-
-// //Single line end
-// // Pre skill drill 13.4.3
-// // Coordinates for each point to be used in the polyline.
-// let line = [
-// 	[33.9416, -118.4085],
-// 	[37.6213, -122.3790],
-// 	[40.7899, -111.9791],
-// 	[47.4502, -122.3088]
-//   ];
-
-// //skill drill 13.4.3
-// Coordinates for each point to be used in the polyline.
-let line = [
-	[37.6213, -122.3790],
-	[37.8968929,-121.2514811],
-	[30.1974711,-97.6663529],
-	[43.6777176,-79.6248197],
-	[40.6413111,-73.7781391]
-  ];
-
-  // Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-	color: "blue",
-	dashArray: "10"
- }).addTo(map);
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // //Alternate method of map object allows adding more properties
 // // Create the map object with a center and zoom level.
@@ -119,9 +76,9 @@ L.polyline(line, {
 
 //   ];
 
-// // not going to be used for line drawing
-// // Get data from cities.js
-// let cityData = cities;
+
+// Get data from cities.js
+let cityData = cities;
 
 // //removed after moving cities array to cities.js file
 // // Loop through the cities array and create one marker for each city.
@@ -144,29 +101,29 @@ L.polyline(line, {
 // 	.addTo(map);
 // });
 
-// //Not going to be used for line drawing...
-// // Add a circle to the map
-// L.circleMarker([34.0522, -118.2437],{
-// 	radius:300,
-// 	color: 'black',
-// 	fillcolor: '#ffffa1',
-// }).addTo(map);
 
-// // Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-// 	console.log(city)
-// 	L.circleMarker(city.location, {
-// 		radius: (city.population -200000)/100000,
-// 		color: '#FF8C00',
-// 		fillcolor: '#FFA500',
-// 		lineWeight: 4
-// 	})
-// 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-// 	.addTo(map);
-// });
+// Add a circle to the map
+L.circleMarker([34.0522, -118.2437],{
+	radius:300,
+	color: 'black',
+	fillcolor: '#ffffa1',
+}).addTo(map);
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+	console.log(city)
+	L.circleMarker(city.location, {
+		radius: (city.population -200000)/100000,
+		color: '#FF8C00',
+		fillcolor: '#FFA500',
+		lineWeight: 4
+	})
+	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+	.addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
